97654ad7d727d364c47b1c9ee898c3e8f32b1de6
Noboru Kobayashi <kobayashi@mediaxis.jp>
2018-12-14 10:54:13 +0900

    update fisher

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,7 @@
-set fish_plugins theme peco
-
-function fish_user_key_bindings
-  bind \cr 'peco_sync_select_history (commandline -b)'
-end
-
-function docker_login
-  docker exec -it $argv[1] bash
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
 end
 
 # bind
@@ -13,29 +9,12 @@ bind -e \cd
 bind \cd delete-char
 
 # alias
-alias g git
-alias docker-login docker_login
 if type gsed > /dev/null 2>&1
   blias sed gsed
 end
-alias noti terminal-notifier
-alias fig docker-compose
-alias docker-exec "docker container exec -it"
-
 
 # env
 set -x EDITOR vim
 
 # rbenv
 rbenv init - | source
-
-# Sync history
-function history-merge --on-event fish_preexec
-    history --save
-    history --merge
-end
-
-function peco_sync_select_history
-    history-merge
-    peco_select_history $argv
-end
